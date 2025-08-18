1e9ba625b6847d300acc8a81d8629395bd19fcea
omzoxima <om.prakash@zoxima.com>
2025-08-18 15:17:25 +0530

    add new field in episode bundle price

--- src/models/episodeBundlePrice.js
+++ src/models/episodeBundlePrice.js
@@ -35,6 +35,10 @@ export default (sequelize) => {
     created_at: {
       type: DataTypes.DATE,
       defaultValue: DataTypes.NOW
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
     }
   }, {
     tableName: 'episode_bundle_prices',
